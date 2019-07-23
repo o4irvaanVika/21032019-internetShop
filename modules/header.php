4b8487f998b27cca7ccd51d9b9d185ca3715cf9d
Виктория Очирваань <o4irvaan@yandex.ru>
2019-07-23 18:59:28 +0300

    ds

--- modules/header.php
+++ modules/header.php
@@ -1,37 +1,52 @@
 <?php
 
-    include($_SERVER['DOCUMENT_ROOT'] . '/config/config.php');
+include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
 
-    // Запрос к БД с просьбой найти все категории где parent_id = 0
-    $query = "SELECT * FROM `categories` WHERE `parent_id` = 0";
-    $result = mysqli_query($link, $query);
+// Запрос к БД с просьбой найти все категории где parent_id = 0
+$query = "SELECT * FROM `categories` WHERE `parent_id` = 0";
+$result = mysqli_query($link, $query);
 
-    $links = '';
-    while ($res = mysqli_fetch_assoc($result)) {
-        $links  .= '<li><a href="/catalog/catalog.php?category='.$res['id'].'">'.$res['name'].'</a></li>';
-    }
+$links = '';
+while ($res = mysqli_fetch_assoc($result)) {
+    $links .= '<li><a href="/catalog/catalog.php?category=' . $res['id'] . '">' . $res['name'] . '</a></li>';
+}
 
-    // формируем ссылку в цикле для каждой строчки, найденной в БД 
-?> 
+// формируем ссылку в цикле для каждой строчки, найденной в БД
+?>
 
+
+
+
+<body>
 <div class="wrapper">
-    
+
     <header>
-        <div class="header-left"></div>
-        <div class="header-center">
-            <ul>
-                <?=$links?>
-                <li><a href="#">Новинки</a></li>
-                <li><a href="#">О нас</a></li>
-            </ul>
+        <div class = "header-first">
+            <div class="header-left"></div>
+            <div class="header-center">
+                <ul>
+                 <?=$links?>
+                 <li><a href="#">Новинки</a></li>
+                 <li><a href="#">О нас</a></li>
+                </ul>
+
+            </div>
         </div>
-        
+
         <div class="header-right">
-         <nav class="user">Привет,Виктория<span>(Выйти)</span></nav>
-         <a class="user-link">корзина(5)</a>
+            <div class="header-right-item">
+                <div class="user"></div>
+                <p>Привет,Виктория</p>
+                <a href = "#"><span>(Выйти)</span></a>
+            </div>
+
+            <div class="header-right-item">
+                <div class="user-link"></div>
+                <a href = "#">корзина(5)</a>
+            </div>
+
         </div>
-  
+        <!-- <div class="burger-menu"></div> -->
+
     </header>
-  
-   
-</div>
+
